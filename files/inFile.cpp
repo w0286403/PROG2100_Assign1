d1411db65a911a522dd1bbf25a62a94ee2c0d5a5
ZackGlenen <w0286403@nscc.ca>
2022-10-06 12:58:54 -0300

    Replacing string working

--- files/inFile.cpp
+++ files/inFile.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-   int x=4;
-   if (x<3) x++;
-   cout << x << endl;
-   return 0;
+    int x=4;
+    if (x < 3) x++;
+    cout << x << endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,37 @@ int main() {
     string fileNameIn = "../files/inFile.cpp";
     string fileNameOut = "../files/outFile.html";
     string line;
+    int index = 0;
+    string bufferString;
+
+    string lessThanString = "&lt;";
+    string greaterThanString = "&gt;";
 
     ifstream inputFile(fileNameIn);
-    ofstream outputFile(fileNameOut,ios::app);
+    ofstream outputFile(fileNameOut,ios::out);
+
+    outputFile << "<PRE>" << endl;
 
     while(!inputFile.eof()){
         getline(inputFile,line);
         cout << line << endl;
-        outputFile << line << endl;
+
+
+        for (int i = 0; i < line.length(); ++i) {
+            if (line[i] == '<'){
+                bufferString += lessThanString;
+            }else if (line[i] == '>'){
+                bufferString += greaterThanString;
+            }else{
+                bufferString += line[i];
+            }
+        }
+
+        outputFile << bufferString << endl;
+        bufferString.clear();
     }
+
+    outputFile << "</PRE>" << endl;
 
     inputFile.close();
     outputFile.close();
@@ -31,3 +53,14 @@ int main() {
  *if < in line change it to &lt
  * if > in line change it to &gt
  */
+
+/*        for (char i : line){
+
+            if (i == '<'){
+               *//* line.replace(index,1,"&lt;");
+                index += (int)lessThanString.length()-1;*//*
+            }
+            if (i == '>'){
+                *//*line.replace(index,1,"&gt;");
+                index += (int)greaterThanString.length()-1;*//*
+            }*/
