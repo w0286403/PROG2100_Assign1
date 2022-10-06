9f2da1b310c8456db072bbee611a07520ef5fabf
ZackGlenen <w0286403@nscc.ca>
2022-10-06 14:00:01 -0300

    Working prototype w/ TODO

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 
 using namespace std;
 int main() {
     string fileNameIn = "../files/inFile.cpp";
     string fileNameOut = "../files/outFile.html";
     string line;
-    int index = 0;
     string bufferString;
-
     string lessThanString = "&lt;";
     string greaterThanString = "&gt;";
+
+
+    cout << "Please place your file inside the \"files\" directory." << endl;
+    cout << "Enter the file name you would like to convert to html (e.g. \"inputFile\"):" << endl;
+    //getline(cin,fileNameIn);
+
+    cout << "Enter the file name for the output (e.g. \"outputFile\")" << endl;
+    //getline(cin,fileNameOut);
 
     ifstream inputFile(fileNameIn);
     ofstream outputFile(fileNameOut,ios::out);
@@ -20,47 +27,35 @@ int main() {
 
     while(!inputFile.eof()){
         getline(inputFile,line);
-        cout << line << endl;
 
-
-        for (int i = 0; i < line.length(); ++i) {
-            if (line[i] == '<'){
+        for (char i : line) {
+            if (i == '<'){
                 bufferString += lessThanString;
-            }else if (line[i] == '>'){
+            }else if (i == '>'){
                 bufferString += greaterThanString;
             }else{
-                bufferString += line[i];
+                bufferString += i;
             }
         }
 
         outputFile << bufferString << endl;
         bufferString.clear();
     }
-
     outputFile << "</PRE>" << endl;
 
     inputFile.close();
     outputFile.close();
+
+    cout << "Your file has been converted to HTML! Please check the \"files\" directory." << endl;
     return 0;
 }
 
-
 /*
- * Read In cpp file line by line
- * Write each line to .html file
- * insert <PRE> as the first line
- * insert </PRE> as the last line
- *if < in line change it to &lt
- * if > in line change it to &gt
+ *TODO
+ * Create regex for valid windows filenames
+ * Ask about interface
+ * Must read only from cpp
+ * Create a custom routine with library string functions (ie find a way to use string.repalce())
+ * Implement fin.fail, fout.fail
+ * 3 try catch blocks around all file read write and close. PROVIDE EXAMPLES OF LIBRARY, DEFAULT, & PROGRAMMER CREATED EXCEPTIONS
  */
-
-/*        for (char i : line){
-
-            if (i == '<'){
-               *//* line.replace(index,1,"&lt;");
-                index += (int)lessThanString.length()-1;*//*
-            }
-            if (i == '>'){
-                *//*line.replace(index,1,"&gt;");
-                index += (int)greaterThanString.length()-1;*//*
-            }*/
