167fe6103bd8339873684516014aa52942a81f3e
w0286403 <w0286403@nscc.ca>
2022-10-07 09:05:15 -0300

    .fail()

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,29 @@
 
 using namespace std;
 int main() {
-    string fileNameIn = "../files/inFile.cpp";
-    string fileNameOut = "../files/outFile.html";
+    string fileNameIn = "inFile";
+    string fileNameOut = "outFile";
     string line;
     string bufferString;
     string lessThanString = "&lt;";
     string greaterThanString = "&gt;";
 
-
     cout << "Please place your file inside the \"files\" directory." << endl;
     cout << "Enter the file name you would like to convert to html (e.g. \"inputFile\"):" << endl;
     //getline(cin,fileNameIn);
+    ifstream inputFile("../files/" + fileNameIn + ".cpp");
+    if (inputFile.fail()){
+        cout << "ERROR - Could not open file " << fileNameIn << endl;
+        cout << "Please make sure it is a .cpp file." << endl;
+    }
 
     cout << "Enter the file name for the output (e.g. \"outputFile\")" << endl;
     //getline(cin,fileNameOut);
+    ofstream outputFile("../files/" + fileNameOut + ".html",ios::out);
+    if (outputFile.fail()){
+        cout << "ERROR - Could not open " << fileNameOut << "for output." << endl;
+    }
 
-    ifstream inputFile(fileNameIn);
-    ofstream outputFile(fileNameOut,ios::out);
 
     outputFile << "<PRE>" << endl;
 
@@ -54,7 +60,6 @@ int main() {
  *TODO
  * Create regex for valid windows filenames
  * Ask about interface
- * Must read only from cpp
  * Create a custom routine with library string functions (ie find a way to use string.repalce())
  * Implement fin.fail, fout.fail
  * 3 try catch blocks around all file read write and close. PROVIDE EXAMPLES OF LIBRARY, DEFAULT, & PROGRAMMER CREATED EXCEPTIONS
