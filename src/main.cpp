3be61c1ac259158977fceb9fc3bc08a282abe313
ZackGlenen <w0286403@nscc.ca>
2022-10-13 01:42:34 -0300

    try

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,20 @@
 #include <string>
 #include <regex>
 using namespace std;
-bool validate(string);
+void validate(string);
+
+struct fileException : public exception{
+public:
+    const string what(){
+        return "File failed to open. Does it Exist and is it a cpp file?";
+    }
+};
 
 int main() {
     string fileNameIn = "inFile";
     string fileNameOut = "outFile";
+    ifstream inputFile;
+    ofstream outputFile;
     string line;
     string bufferString;
     string lessThanString = "&lt;";
@@ -15,69 +24,72 @@ int main() {
     bool willContinue = true;
 
     cout << "Please place your file inside the \"files\" directory." << endl;
-    cout << "Enter the file name you would like to convert to html (e.g. \"inputFile\"):" << endl;
-    getline(cin,fileNameIn);
-    willContinue = validate(fileNameIn);
-    if (!willContinue){
-        cout << "ERROR" << endl;
-    }
-    ifstream inputFile("../files/" + fileNameIn + ".cpp");
-    if (inputFile.fail()){
-        cout << "ERROR - Could not open file " << fileNameIn << endl;
-        cout << "Please make sure it is a .cpp file." << endl;
-    }
 
-    cout << "Enter the file name for the output (e.g. \"outputFile\")" << endl;
-    getline(cin,fileNameOut);
-    ofstream outputFile("../files/" + fileNameOut + ".html",ios::out);
-    if (outputFile.fail()){
-        cout << "ERROR - Could not open " << fileNameOut << "for output." << endl;
-    }
-
-
-    outputFile << "<PRE>" << endl;
-
-    while(!inputFile.eof()){
-        getline(inputFile,line);
-
-        for (char i : line) {
-            if (i == '<'){
-                bufferString += lessThanString;
-            }else if (i == '>'){
-                bufferString += greaterThanString;
-            }else{
-                bufferString += i;
+    do {
+        try {
+            cout << "Enter the file name you would like to convert to html (e.g. \"inputFile\"):" << endl;
+            getline(cin,fileNameIn);
+            validate(fileNameIn);
+            inputFile.open("../files/" + fileNameIn + ".cpp");
+            if (inputFile.fail()){
+                throw fileException();
             }
+
+            cout << "Enter the file name for the output (e.g. \"outputFile\")" << endl;
+            getline(cin,fileNameOut);
+            validate(fileNameOut);
+            outputFile.open("../files/" + fileNameOut + ".html",ios::out);
+            if (outputFile.fail()){
+                throw fileException();
+            }
+        }catch (fileException& ex){
+            cout << ex.what() << endl;
+            willContinue = false;
+        }catch (const invalid_argument& e){
+            cout << e.what() << endl;
+            willContinue = false;
+        }catch (...){
+            willContinue = false;
         }
+    }while(!willContinue);
 
-        outputFile << bufferString << endl;
-        bufferString.clear();
+    try{
+        outputFile << "<PRE>" << endl;
+
+        while(!inputFile.eof()){
+            getline(inputFile,line);
+
+            for (char i : line) {
+                if (i == '<'){
+                    bufferString += lessThanString;
+                }else if (i == '>'){
+                    bufferString += greaterThanString;
+                }else{
+                    bufferString += i;
+                }
+            }
+
+            outputFile << bufferString << endl;
+            bufferString.clear();
+        }
+        outputFile << "</PRE>" << endl;
+
+        inputFile.close();
+        outputFile.close();
+    }catch(...){
+
     }
-    outputFile << "</PRE>" << endl;
-
-    inputFile.close();
-    outputFile.close();
-
     cout << "Your file has been converted to HTML! Please check the \"files\" directory." << endl;
     return 0;
 }
 
-bool validate(string fileName){
-    bool willContinue = true;
+void validate(string fileName){
+
     if (regex_match(fileName,regex("CON|PRN|AUX|NUL|COM1|COM2|COM3|COM4|COM5|COM6|COM7"
                                    "|COM8|COM9|LPT1|LPT2|LPT3|LPT4|LPT5|LPT6|LPT7|LPT8|LPT9"))){
-        willContinue = false;
+        throw invalid_argument("This file name is being used by the system.");
     }else if(!regex_match(fileName,regex("[a-zA-Z_]\\s?[a-zA-Z_0-9]*"))){
-        willContinue = false;
+        throw invalid_argument("Received unacceptable file character.");
     }
     //[a-zA-Z_]\s?[a-zA-Z_0-9]*.[a-zA-Z0-9]+ REGEX FOR FILE TYPES
-
-    return willContinue;
 }
-
-/*
- *TODO
- * Create regex for valid windows filenames
- * Ask about interface
- * 3 try catch blocks around all file read write and close. PROVIDE EXAMPLES OF LIBRARY, DEFAULT, & PROGRAMMER CREATED EXCEPTIONS
- */
