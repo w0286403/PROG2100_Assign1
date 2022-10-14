2a451e8f083691f6eb4084c0f9fd80ecb1b88e31
w0286403 <w0286403@nscc.ca>
2022-10-14 10:19:29 -0300

    xxxx

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,23 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <stdexcept>
 using namespace std;
 void validate(string);
 
 struct fileException : public exception{
 public:
+    string message;
+    fileException(string messageIn){
+        this->message = messageIn;
+    }
     const string what(){
-        return "File failed to open. Does it Exist and is it a cpp file?";
+        return "File failed to open. Does it Exist?" + message;
     }
 };
 
 int main() {
+    string fileReg = "E:\\ITProgrammingFall2022\\PROG\\Assignment1\\files\\inFile.cpp";
     string fileNameIn = "inFile";
     string fileNameOut = "outFile";
     ifstream inputFile;
@@ -23,24 +29,25 @@ int main() {
     string greaterThanString = "&gt;";
     bool willContinue = true;
 
-    cout << "Please place your file inside the \"files\" directory." << endl;
+    cout << "\nFILE CONVERTER\n" << endl;
+    cout << "Please make sure your file path includes a drive letter.\nDo not include the file type." << endl;
 
     do {
         try {
-            cout << "Enter the file name you would like to convert to html (e.g. \"inputFile\"):" << endl;
+            cout << R"(Enter the path to the file you would like to convert to html. (e.g. C:\files\inputFile):)" << endl;
             getline(cin,fileNameIn);
             validate(fileNameIn);
-            inputFile.open("../files/" + fileNameIn + ".cpp");
+            inputFile.open(fileNameIn + ".cpp");
             if (inputFile.fail()){
-                throw fileException();
+                throw fileException("asd");
             }
 
-            cout << "Enter the file name for the output (e.g. \"outputFile\")" << endl;
+            cout << R"(Enter the path and name for the output html file. (e.g. C:\files\outputFile))" << endl;
             getline(cin,fileNameOut);
             validate(fileNameOut);
-            outputFile.open("../files/" + fileNameOut + ".html",ios::out);
+            outputFile.open(fileNameOut + ".html",ios::out);
             if (outputFile.fail()){
-                throw fileException();
+                throw fileException("aaaaaaaa");
             }
         }catch (fileException& ex){
             cout << ex.what() << endl;
@@ -79,17 +86,17 @@ int main() {
     }catch(...){
 
     }
-    cout << "Your file has been converted to HTML! Please check the \"files\" directory." << endl;
+    cout << "Your file has been converted to HTML! Please check your directory." << endl;
     return 0;
 }
 
 void validate(string fileName){
 
-    if (regex_match(fileName,regex("CON|PRN|AUX|NUL|COM1|COM2|COM3|COM4|COM5|COM6|COM7"
+/*    if (regex_match(fileName,regex("CON|PRN|AUX|NUL|COM1|COM2|COM3|COM4|COM5|COM6|COM7"
                                    "|COM8|COM9|LPT1|LPT2|LPT3|LPT4|LPT5|LPT6|LPT7|LPT8|LPT9"))){
-        throw invalid_argument("This file name is being used by the system.");
-    }else if(!regex_match(fileName,regex("[a-zA-Z_]\\s?[a-zA-Z_0-9]*"))){
-        throw invalid_argument("Received unacceptable file character.");
+        throw invalid_argument("This file name is being used by the system. Please choose another name.");
+    }else*/
+    if(!regex_match(fileName,regex(R"([a-zA-Z]\:[a-zA-Z0-9\s?_\\]*)"))){
+        throw invalid_argument("Error in file path.\n Please make sure you have a drive letter, all characters are valid, and there is no file type.\n (e.g. C:\\files\\inFile)");
     }
-    //[a-zA-Z_]\s?[a-zA-Z_0-9]*.[a-zA-Z0-9]+ REGEX FOR FILE TYPES
 }
