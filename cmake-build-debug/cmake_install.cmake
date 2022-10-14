b05b00eb9c559055be1fc52cb8aa4c6190498e1f
ZackGlenen <w0286403@nscc.ca>
2022-10-14 15:52:44 -0300

    Assign 1 Complete

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/w0286403/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/222.4167.35/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2021.3.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,42 @@
 #include <regex>
 #include <stdexcept>
 using namespace std;
-void validate(string);
+void validate(string); // P Declaration for validate
 
-struct fileException : public exception{
+struct fileException : public exception{ //Create file handling exception that will accept a message depending on scenario
 public:
     string message;
     fileException(string messageIn){
         this->message = messageIn;
     }
     const string what(){
-        return "File failed to open. Does it Exist?" + message;
+        return "File has failed. " + message;
     }
 };
 
 int main() {
-    string fileReg = "E:\\ITProgrammingFall2022\\PROG\\Assignment1\\files\\inFile.cpp";
-    string fileNameIn = "inFile";
-    string fileNameOut = "outFile";
+    //PATH = E:\ITProgrammingFall2022\PROG\Assignment1\files\inFile
+    string fileNameIn;// = "inFile";
+    string fileNameOut;// = "outFile";
     ifstream inputFile;
     ofstream outputFile;
-    string line;
-    string bufferString;
-    string lessThanString = "&lt;";
+    string currentLine;
+    string bufferLine;
+    string lesserThanString = "&lt;";
     string greaterThanString = "&gt;";
-    bool willContinue = true;
+    bool willContinue = false;
 
-    cout << "\nFILE CONVERTER\n" << endl;
-    cout << "Please make sure your file path includes a drive letter.\nDo not include the file type." << endl;
+    cout << "\nFILE CONVERTER cpp -> html\n" << endl;
+    cout << "Please make sure your file path includes a drive letter. Please do not include the file type.\n" << endl;
 
     do {
-        try {
+        try {//Try to grab filenames, validate against regex, and open
             cout << R"(Enter the path to the file you would like to convert to html. (e.g. C:\files\inputFile):)" << endl;
             getline(cin,fileNameIn);
             validate(fileNameIn);
             inputFile.open(fileNameIn + ".cpp");
-            if (inputFile.fail()){
-                throw fileException("asd");
+            if (inputFile.fail()){ //If files fail, throw exception
+                throw fileException("Please check spelling and make sure it is a cpp file.");
             }
 
             cout << R"(Enter the path and name for the output html file. (e.g. C:\files\outputFile))" << endl;
@@ -47,46 +47,52 @@ int main() {
             validate(fileNameOut);
             outputFile.open(fileNameOut + ".html",ios::out);
             if (outputFile.fail()){
-                throw fileException("aaaaaaaa");
+                throw fileException("An error occurred with the output file.");
             }
+            willContinue = true;
+        }catch (const invalid_argument& ex) {//Invalid argument through validate function
+            cout << ex.what() << endl;
         }catch (fileException& ex){
             cout << ex.what() << endl;
-            willContinue = false;
-        }catch (const invalid_argument& e){
-            cout << e.what() << endl;
-            willContinue = false;
         }catch (...){
-            willContinue = false;
+            cout << "An unexpected error has occurred." << endl;
         }
     }while(!willContinue);
 
-    try{
+    try{//Try-catch for reading through input line
         outputFile << "<PRE>" << endl;
 
-        while(!inputFile.eof()){
-            getline(inputFile,line);
-
-            for (char i : line) {
-                if (i == '<'){
-                    bufferString += lessThanString;
-                }else if (i == '>'){
-                    bufferString += greaterThanString;
-                }else{
-                    bufferString += i;
+        while(!inputFile.eof()){ //While there is data in file, read dit line by line
+            if (!getline(inputFile, currentLine)){
+                throw fileException("");
+            }else {
+                for (char i: currentLine) { //For each character in the line, check if it is < or >
+                    //Replace where applicable and append to buffer string
+                    if (i == '<') {
+                        bufferLine += lesserThanString;
+                    } else if (i == '>') {
+                        bufferLine += greaterThanString;
+                    } else {
+                        bufferLine += i;
+                    }
                 }
-            }
 
-            outputFile << bufferString << endl;
-            bufferString.clear();
+                outputFile << bufferLine << endl;//write buffer string to outputfile
+                bufferLine.clear();//clear the string to read new line
+            }
         }
         outputFile << "</PRE>" << endl;
 
         inputFile.close();
         outputFile.close();
-    }catch(...){
+        cout << "Your file has been converted to HTML! Please check your directory." << endl;
 
+    }catch (fileException& ex){
+        cout << ex.what() << endl;
     }
-    cout << "Your file has been converted to HTML! Please check your directory." << endl;
+    catch(...){
+        cout << "An unknown error occurred." << endl;
+    }
     return 0;
 }
 
@@ -96,7 +102,7 @@ void validate(string fileName){
                                    "|COM8|COM9|LPT1|LPT2|LPT3|LPT4|LPT5|LPT6|LPT7|LPT8|LPT9"))){
         throw invalid_argument("This file name is being used by the system. Please choose another name.");
     }else*/
-    if(!regex_match(fileName,regex(R"([a-zA-Z]\:[a-zA-Z0-9\s?_\\]*)"))){
+    if(!regex_match(fileName,regex(R"([a-zA-Z]\:[a-zA-Z0-9\s?_\\]*)"))){//Regex matches drive letter, and full directory path. user must not enter file type
         throw invalid_argument("Error in file path.\n Please make sure you have a drive letter, all characters are valid, and there is no file type.\n (e.g. C:\\files\\inFile)");
     }
 }
